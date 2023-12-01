839f49c176e3636db466485ed91152a139de6bf0
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-11-30 22:27:43 -0500

    Change user from string to model
    Initially the user in the database is represented by strings now it is changed to a account model

--- server/init.js
+++ server/init.js
@@ -3,25 +3,18 @@
 // Script should take admin credentials as arguments as described in the requirements doc.
 let userArgs = process.argv.slice(2);
 
-// if (!userArgs[0].startsWith('mongodb')) {
-//     console.log('ERROR: You need to specify a valid mongodb URL as the first argument');
-//     return
-// }
-
 let Tag = require('./models/tags')
 let Answer = require('./models/answers')
 let Question = require('./models/questions')
-let User = require('./modes/account')
+let User = require('./models/account')
 
 let mongoose = require('mongoose');
-let mongoDB = 'mongodb://127.0.0.1:27017/fake_so'; // pre-determine 
+let mongoDB = 'mongodb://127.0.0.1:27017/fake_so'; // pre-determine local database
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 // mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-let tags = [];
-let answers = [];
 function tagCreate(name) {
   let tag = new Tag({ name: name });
   return tag.save();
@@ -75,20 +68,25 @@ function userCreate(username, password,email){
 }
 
 const populate = async () => {
-  await adminCreate(userArgs[1], userArgs2[2]);
-  let u1 = await userCreate("Joe", "123455", "joe@gmail.com");
-  let u2 = await userCreate("John", "abcdefg", "john@gmail.com");
+  await adminCreate(userArgs[1], userArgs[2], "admin@gmail.com");
+  let u1 = await userCreate('hamkalo', '123455', 'hamkalo@gmail.com');
+  let u2 = await userCreate('azad', 'abcdefg', 'azad@gmail.com');
+  let u3 = await userCreate('abaya', 'fdasfdsa', 'abaya@gmail.com');
+  let u4 = await userCreate('alia', 'rejakdjf;', 'alia@gmail.com');
+  let u5 = await userCreate('sana', 'r0u3jajfa', 'sana@gmail.com');
+  let u6 = await userCreate('Joji John', '34iordsja', 'JojiJohn@gmail.com');
+  let u7 = await userCreate('saltyPeter', 'fdlkaj2rja', 'saltyPeter@gmail.com');
   let t1 = await tagCreate('react');
   let t2 = await tagCreate('javascript');
   let t3 = await tagCreate('android-studio');
   let t4 = await tagCreate('shared-preferences');
-  let a1 = await answerCreate('React Router is mostly a wrapper around the history library. history handles interaction with the browser\'s window.history for you with its browser and hash histories. It also provides a memory history which is useful for environments that don\'t have a global history. This is particularly useful in mobile app development (react-native) and unit testing with Node.', 'hamkalo', false);
-  let a2 = await answerCreate('On my end, I like to have a single history object that I can carry even outside components. I like to have a single history.js file that I import on demand, and just manipulate it. You just have to change BrowserRouter to Router, and specify the history prop. This doesn\'t change anything for you, except that you have your own history object that you can manipulate as you want. You need to install history, the library used by react-router.', 'azad', false);
-  let a3 = await answerCreate('Consider using apply() instead; commit writes its data to persistent storage immediately, whereas apply will handle it in the background.', 'abaya', false);
-  let a4 = await answerCreate('YourPreference yourPrefrence = YourPreference.getInstance(context); yourPreference.saveData(YOUR_KEY,YOUR_VALUE);', 'alia', false);
-  let a5 = await answerCreate('I just found all the above examples just too confusing, so I wrote my own. ', 'sana', false);
-  await questionCreate('Programmatically navigate using React router', 'the alert shows the proper index for the li clicked, and when I alert the variable within the last function I\'m calling, moveToNextImage(stepClicked), the same value shows but the animation isn\'t happening. This works many other ways, but I\'m trying to pass the index value of the list item clicked to use for the math to calculate.', [t1, t2], [a1, a2], 'Joji John', false, false);
-  await questionCreate('android studio save string shared preference, start activity and load the saved string', 'I am using bottom navigation view but am using custom navigation, so my fragments are not recreated every time i switch to a different view. I just hide/show my fragments depending on the icon selected. The problem i am facing is that whenever a config change happens (dark/light theme), my app crashes. I have 2 fragments in this activity and the below code is what i am using to refrain them from being recreated.', [t3, t4, t2], [a3, a4, a5], 'saltyPeter', false, 121);
+  let a1 = await answerCreate('React Router is mostly a wrapper around the history library. history handles interaction with the browser\'s window.history for you with its browser and hash histories. It also provides a memory history which is useful for environments that don\'t have a global history. This is particularly useful in mobile app development (react-native) and unit testing with Node.', u1, false);
+  let a2 = await answerCreate('On my end, I like to have a single history object that I can carry even outside components. I like to have a single history.js file that I import on demand, and just manipulate it. You just have to change BrowserRouter to Router, and specify the history prop. This doesn\'t change anything for you, except that you have your own history object that you can manipulate as you want. You need to install history, the library used by react-router.', u2, false);
+  let a3 = await answerCreate('Consider using apply() instead; commit writes its data to persistent storage immediately, whereas apply will handle it in the background.', u3, false);
+  let a4 = await answerCreate('YourPreference yourPrefrence = YourPreference.getInstance(context); yourPreference.saveData(YOUR_KEY,YOUR_VALUE);', u4, false);
+  let a5 = await answerCreate('I just found all the above examples just too confusing, so I wrote my own. ', u5, false);
+  await questionCreate('Programmatically navigate using React router', 'the alert shows the proper index for the li clicked, and when I alert the variable within the last function I\'m calling, moveToNextImage(stepClicked), the same value shows but the animation isn\'t happening. This works many other ways, but I\'m trying to pass the index value of the list item clicked to use for the math to calculate.', [t1, t2], [a1, a2], u6, false, false);
+  await questionCreate('android studio save string shared preference, start activity and load the saved string', 'I am using bottom navigation view but am using custom navigation, so my fragments are not recreated every time i switch to a different view. I just hide/show my fragments depending on the icon selected. The problem i am facing is that whenever a config change happens (dark/light theme), my app crashes. I have 2 fragments in this activity and the below code is what i am using to refrain them from being recreated.', [t3, t4, t2], [a3, a4, a5], u7, false, 121);
   if(db) db.close();
   console.log('done');
 }
