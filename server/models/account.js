89a5579a56b384857ab4d7ec16077b6b04936748
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-12-01 20:13:56 -0500

    Make username and password required

--- server/models/account.js
+++ server/models/account.js
@@ -6,9 +6,9 @@ var Schema = mongoose.Schema;
 
 // a session model that stores the informations about users
 var userSchema = new Schema({
-    username: {type: String},
-    password: {type: String},
-    email: {type: String},
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
     reputation: {type: Number, default: 0},
     role: {type: String, default: 'normal'}
 });
