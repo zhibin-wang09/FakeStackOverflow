f36bbbf2a70bcc0c5445c3868c7416b25242703a
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-12-03 15:02:01 -0500

    Create handler to add comment to answers, increase comment vote, decrease comment vote

--- server/controllers/comment_controller.js
+++ server/controllers/comment_controller.js
@@ -1,8 +1,9 @@
 const comment = require('../models/comment');
 const question = require('../models/questions');
+const answer = require('../models/answers')
 const user = require('../models/account')
 
-const postcomment = async (req,res) => { // a post method to create a new comment for the question
+const postCommentToQuestion = async (req,res) => { // a post method to create a new comment for the question
     const q = await question.findOne({_id: req.params.id}); // get the corresponding question to add comment to
     const u = await user.findOne({email : req.body.email}); // get the user who is making the comment
     const c = await comment.create({text: req.body.text, user: u}); // create the new comment
@@ -10,4 +11,22 @@ const postcomment = async (req,res) => { // a post method to create a new commen
     res.status(200).send(c);
 }
 
-module.exports = {postcomment};
+const postCommentToAnswer = async (req,res) => { // a post method to create a new comment for the answer
+    const a = await answer.findOne({_id: req.params.id}); // get the corresponding answer to add comment to
+    const u = await user.findOne({email : req.body.email}); // get the user who is making the comment
+    const c = await comment.create({text: req.body.text, user: u}); // create the new comment
+    await answer.updateOne({_id : req.params.id}, {comment : [...a[0].comment, c]}); // add the comment to the question
+    res.status(200).send(c);
+}
+
+const increaseCommentVote = async (req,res) => {
+    const c = await comment.findOne({_id : req.params.id});
+    await comment.updateOne({_id: req.params.id}, {votes: c.votes + 1});
+}
+
+const decreaseCommentVote = async (req,res) => {
+    const c = await comment.findOne({_id : req.params.id});
+    await comment.updateOne({_id: req.params.id}, {votes: c.votes - 1});
+}
+
+module.exports = {postCommentToQuestion,postCommentToAnswer, increaseCommentVote, decreaseCommentVote};
