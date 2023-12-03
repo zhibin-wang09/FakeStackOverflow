8b21d2c764daf9f77468dcaebd28d3900abbf70d
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-12-03 13:02:17 -0500

    Add route for authentication

--- server/routers/fake_so_router.js
+++ server/routers/fake_so_router.js
@@ -4,6 +4,7 @@ const {postQuestion, getQuestionByKeyword ,getQuestion
 ,updateView, getQuestionById} = require('../controllers/question_controller'); // crud methods for questions
 const {getAnswers, postAnswers} = require('../controllers/answer_controller'); // crud methods for answers
 const {getTag} = require('../controllers/tag_controller'); // crud method for tags
+const {signup, login, logout, verify} = require('../controllers/account_controller'); // has all the authroization & authentication functions
 const router = express.Router();
 
 router.route('/get/questions').get(getQuestion); // retrieve questions
@@ -14,5 +15,8 @@ router.route('/get/answers/:id').get(getAnswers); // retrieve answers asscoiated
 router.route('/post/answer/:id').post(postAnswers); // add brand new answer
 router.route('/post/questions/updateview/:id').post(updateView); // update the # of views of a question
 router.route('/get/tags').get(getTag); // retrieve all the tags
+router.route('/login').post(login);
+router.route('/signup').post(signup);
+router.route('/logout').get(logout);
 
 module.exports = router;
