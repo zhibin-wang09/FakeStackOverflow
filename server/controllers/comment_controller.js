cc00e290ec8547351519ea87de80c7fc423573bf
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-12-09 17:12:57 -0500

    upvote comment

--- server/controllers/comment_controller.js
+++ server/controllers/comment_controller.js
@@ -34,12 +34,33 @@ const postCommentToAnswer = async (req,res) => { // a post method to create a ne
 const increaseCommentVote = async (req,res) => {
     const c = await comment.findOne({_id : req.params.id});
     await comment.updateOne({_id: req.params.id}, {votes: c.votes + 1});
-    res.status(200).send();
+    if(req.body.isQuestion){
+        let q = await question.findOne({_id: req.body.id}).populate('comment').populate('asked_by');
+        q.asked_by.password = null;
+        q.asked_by.email = null;
+        res.status(200).send(q);
+    }else{
+        let a = await answer.findOne({_id: req.body.id}).populate('comment').populate('ans_by');
+        a.ans_by.password = null;
+        a.ans_by.email = null;
+        res.status(200).send(a);
+    }
 }
 
 const decreaseCommentVote = async (req,res) => {
     const c = await comment.findOne({_id : req.params.id});
     await comment.updateOne({_id: req.params.id}, {votes: c.votes - 1});
+    if(req.body.isQuestion){
+        let q = await question.findOne({_id: req.body.id}).populate('comment').populate('asked_by');
+        q.asked_by.password = null;
+        q.asked_by.email = null;
+        res.status(200).send(q);
+    }else{
+        let a = await answer.findOne({_id: req.body.id}).populate('comment').populate('ans_by');
+        a.ans_by.password = null;
+        a.ans_by.email = null;
+        res.status(200).send(a);
+    }
     res.status(200).send();
 }
 
@@ -49,4 +70,4 @@ const deleteComment = async (req,res) => {
     res.status(200).send();
 }
 
-module.exports = {postCommentToQuestion,postCommentToAnswer, increaseCommentVote, decreaseCommentVote};
+module.exports = {postCommentToQuestion,postCommentToAnswer, increaseCommentVote, decreaseCommentVote,deleteComment};
